8076de65103dd29490a0d2f7e51d67e5e3676256
RedsLovesGames <75150137+RedsLovesGames@users.noreply.github.com>
2026-09-25 17:17:18 -0400

    test: require all 13 Toolbox cards

--- tests/toolbox-shell.test.mjs
+++ tests/toolbox-shell.test.mjs
@@ -6,14 +6,14 @@ const html = readFileSync(new URL('../tools/index.html', import.meta.url), 'utf8
 const js = readFileSync(new URL('../tools/shared/toolbox.js', import.meta.url), 'utf8');
 
 test('toolbox index links all shipped primary tools', () => {
-  for (const route of ['./time/', './image/', './money/', './text/', './pdf/', './files/', './media/', './data/']) assert.match(html, new RegExp(`href="${route.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')}"`));
+  for (const route of ['./time/', './image/', './money/', './text/', './pdf/', './files/', './media/', './data/', './developer/', './math/', './random/', './codes/', './network/']) assert.match(html, new RegExp(`href="${route.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')}"`));
 });
 
 test('toolbox exposes favorites and recent tools', () => {
   assert.match(html, /id="favoritesOnly"/);
   assert.match(html, /id="recentTools"/);
   assert.match(html, /data-favorite/);
-  assert.match(html, /'\.\/data\/':'Data'/);
+  for (const pair of ["'./data/':'Data'", "'./random/':'Random'", "'./codes/':'Codes'", "'./network/':'Network'"]) assert.match(html, new RegExp(pair.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')));
   assert.match(js, /toggleFavorite/);
   assert.match(js, /recordRecent/);
 });
